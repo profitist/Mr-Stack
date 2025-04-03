1b20190a360a34b742a6ae5be25b3b84a8fff1b1
Ivan Mikhaylov <ivanmikhaylov.sesc@gmail.com>
2025-04-03 13:57:35 +0500

    Add animation for dino add box object/ settings in box collider add physics for dino

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -23,29 +23,44 @@ public class Player : MonoBehaviour
     [Obsolete("Obsolete")]
     private void FixedUpdate()
     {
-        if (isGrounded && GameInput.Instance.IsJump() && !isJumping)
+        if (isGrounded && GameInput.Instance.IsJump() && !isJumping && Math.Abs(rb.velocity.y) <= 1e-2)
         {
             rb.AddForce(Vector2.up * (Time.fixedDeltaTime * jumpForce) , ForceMode2D.Impulse);
             isJumping = true;
         }
         var movementVector = GameInput.Instance.GetMovementVector();
         movementVector = movementVector.normalized;
-        rb.velocity = new Vector2(movementVector.x * movingSpeed,rb.velocity.y);
+        rb.velocity = new Vector2(movementVector.x * movingSpeed, rb.velocity.y);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (IsGroundedCollision(collision))
         {
             Debug.Log("Collision");
             isGrounded = true;
             isJumping = false;
         }
+        
+    }
+    
+    private bool IsGroundedCollision(Collision2D collision)
+    {
+        // Проходим по всем точкам контакта
+        foreach (ContactPoint2D contact in collision.contacts)
+        {
+            // Если нормаль направлена вверх (с допустимым отклонением)
+            if (contact.normal.y >= 1f) // 0.7f ≈ 45 градусов
+            {
+                return true;
+            }
+        }
+        return false;
     }
     
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (IsGroundedCollision(collision))
         {
             Debug.Log("No Collision");
             isGrounded = false;
