ea4fc767d94d37ae968a6242521eedcd5aeaaf5a
Богдан <ronodar233@gmail.com>
2025-04-18 17:08:54 +0300

    Git(Porn)Hub

--- Assets/Player/BoxHolding.cs
+++ Assets/Player/BoxHolding.cs
@@ -1,21 +1,32 @@
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using Unity.Mathematics.Geometry;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Debug = UnityEngine.Debug;
 
 public class PlayerBoxHolder : MonoBehaviour
 {
     private const int maxBoxCount = 3;
     private Stack<GameObject> boxes;
+    private GameObject NearestBox;
     private Stopwatch wait = new Stopwatch();
     public HashSet<GameObject> ActiveBoxes { get; private set; }
+    public List<GameObject> AllBoxes { get; private set; }
     public Transform holdPoint;
     public static PlayerBoxHolder Instance { get; private set; }
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
+        AllBoxes = new List<GameObject>();
+        foreach (var collider in CapsuleCollider2D.FindObjectsByType<GameObject>(default))
+        {
+            if (collider.CompareTag("Box"))
+                AllBoxes.Add(collider.gameObject);
+        }
         boxes = new Stack<GameObject>();
         holdPoint = GetComponent<Transform>();
         Debug.Log($"{holdPoint.position.y}");
@@ -28,9 +39,9 @@ public class PlayerBoxHolder : MonoBehaviour
     {
         if (wait.IsRunning && wait.ElapsedMilliseconds >= 500)
             wait = new Stopwatch();
-        if (GameInput.Instance.GrabingBox && maxBoxCount > boxes.Count && Player.Instance.NearestBox && !wait.IsRunning)
+        if (GameInput.Instance.GrabingBox && maxBoxCount > boxes.Count && !wait.IsRunning)
         {
-            PickUpBox(Player.Instance.NearestBox);
+            PickUpBox();
             Debug.Log(boxes.Count);
         }
 
@@ -39,16 +50,20 @@ public class PlayerBoxHolder : MonoBehaviour
 
     }
     
-    private void PickUpBox(GameObject box)
+    private void PickUpBox()
     {
+        FindNearestBox();
+        var box = NearestBox;
+        if (box is null)
+            return;
         wait.Start();
+        var capsuleCollider = Player.Instance.GetComponent<CapsuleCollider2D>();
+        capsuleCollider.offset += new Vector2(0, 0.5f);
+        capsuleCollider.size = new Vector2(capsuleCollider.size.x, capsuleCollider.size.y + 1);
         var rb = box.GetComponent<Rigidbody2D>();
         if (rb) rb.simulated = false;
         box.transform.SetParent(holdPoint);
         box.transform.localPosition = new Vector3(0, boxes.Count * 1, 0);
-        var capsuleCollider = Player.Instance.GetComponent<CapsuleCollider2D>();
-        capsuleCollider.offset += new Vector2(0, 0.5f);
-        capsuleCollider.size = new Vector2(capsuleCollider.size.x, capsuleCollider.size.y + 1);
         ActiveBoxes.Add(box);
         boxes.Push(box);
         
@@ -67,5 +82,20 @@ public class PlayerBoxHolder : MonoBehaviour
                   + Player.Instance.rb.linearVelocity.x;
         rb.linearVelocity = new Vector2(vel, 6);
         ActiveBoxes.Remove(box);
+    }
+    
+    public void FindNearestBox()
+    {
+        var boxCollider = AllBoxes
+            .FirstOrDefault(x => !ActiveBoxes.Contains(x) 
+                                 && Vector2.Distance(x.transform.position, Player.Instance.transform.position) < 2
+                                 && System.Math.Abs(x.transform.position.y - Player.Instance.transform.position.y) < 0.5f);
+        if (boxCollider is not null)
+        {
+            NearestBox = boxCollider;
+            Debug.Log("FOUND!");
+        }
+        else
+            NearestBox = null;
     }
 }
