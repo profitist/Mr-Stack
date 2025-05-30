ec857bd1f58c6a48a98d19a064ba157fef92b1c6
Богдан <ronodar233@gmail.com>
2025-05-30 22:19:32 +0300

    clouds

--- Assets/Environment/Scripts/Background Paralax.cs
+++ Assets/Environment/Scripts/Background Paralax.cs
@@ -1,18 +1,33 @@
-﻿using Unity.VisualScripting;
+﻿using System.Diagnostics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Background_Paralax : MonoBehaviour
 {
     public GameObject Instance{get; private set;}
     public Rigidbody2D rb{get; private set;}
+
+    private Stopwatch wait = new Stopwatch();
     private void Awake()
     {
         Instance = this.gameObject;
         rb = gameObject.GetComponent<Rigidbody2D>();
+        wait.Start();
     }
 
     private void FixedUpdate()
     {
-        rb.linearVelocityX = (Player.Instance.rb.linearVelocityX != 0 ) ? -Player.Instance.rb.linearVelocityX / 10 : 0;
+        if (wait.ElapsedMilliseconds <= 1000)
+        {
+            rb.linearVelocityX = 0.3f;
+        }
+        else if (wait.ElapsedMilliseconds <= 2000)
+        {
+            rb.linearVelocityX = -0.3f;
+        }
+        else
+        {
+            wait.Restart();
+        }
     }
 }
